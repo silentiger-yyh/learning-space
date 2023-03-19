72b10c90a3ddf9017e6d88c3811a8a0448bd05c9
喻云虎 <50434721+15623572586@users.noreply.github.com>
2023-03-19 19:11:18 +0800

    submit

--- md标题降级.py
+++ md标题降级.py
@@ -1,4 +1,4 @@
-fileName = r'D:\LearningSpace\Java后端\Java技术栈学习笔记\02_数据库\TDEngine\tdengine操作.md'
+fileName = r'新建 文本文档.md'
 with open(file=fileName, mode="r", encoding='utf-8') as f1:
     lines = f1.readlines()
     for i in range(0, len(lines)):
@@ -6,7 +6,7 @@ with open(file=fileName, mode="r", encoding='utf-8') as f1:
             lines[i] = '#' + lines[i]
 
 #将处理过的lines写入新的文件中
-fileName2 = r'D:\LearningSpace\Java后端\Java技术栈学习笔记\02_数据库\TDEngine\tdengine操作-new.md'
+fileName2 = r'tmp.md'
 with open(file = fileName2,mode='w',encoding='utf-8') as f2:
     f2.writelines(lines)
 
